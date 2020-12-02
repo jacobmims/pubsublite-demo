db8aeaa7fc27d4aec99cbc9633315f9a82fab608
Jacob Mims <jacobmims>
2020-12-02 17:10:51 +0000

    Initial commit

---
+++ src/main/java/com/micronaut/gcp/pubsublite/Application.java
@@ -0,0 +1,10 @@
+package com.micronaut.gcp.pubsublite;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
